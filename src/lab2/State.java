2de13ab274e8b483c0444ae8af9b429870b50c23
Wasyl Franchuk <WasylF@gmail.com>
2014-10-07 16:49:00 +0300

    Update class State

--- src/lab2/State.java
+++ src/lab2/State.java
@@ -6,6 +6,7 @@
 package lab2;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  *
@@ -20,9 +21,104 @@ public class State {
     /**
      * states, that able to be visited from current state
      */
-    private Map<Character, Integer> next;
+    private Map<Character, State> nextStates;
     /**
      * if state is final variable= true, else false
      */
-    private boolean isFinal;
+    private final boolean isFinal;
+
+    public State(int n0, boolean isFinal0) {
+        n = n0;
+        isFinal = isFinal0;
+        nextStates = new TreeMap<Character, State>();
+    }
+
+    /**
+     * add new state, in wich we can go from current by the letter
+     *
+     * @param c letter
+     * @param state - state
+     */
+    public void addNextState(Character c, State state) {
+        nextStates.put(c, state);
+    }
+
+    /**
+     * @return number of state
+     */
+    public int getNumber() {
+        return n;
+    }
+
+    /**
+     * @return number of states that next to current (including current if it
+     * has eps-conversion)
+     */
+    public int getNumberOfConnectedStates() {
+        return nextStates.size();
+    }
+
+    /**
+     * @return if state is fainale return true, else return false
+     */
+    public boolean isFinal() {
+        return isFinal;
+    }
+
+    /**
+     * @param ch letter to conversion from current to next sate
+     * @return state that connected with current by latter ch if it doesn't
+     * exist return null
+     */
+    public State getNextState(Character ch) {
+        if (!nextStates.containsKey(ch)) {
+            return null;
+        }
+        return nextStates.get(ch);
+    }
+
+    /**
+     * @return string, that discribing state
+     */
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder("state " + n + ": ");
+        for (Character character : nextStates.keySet()) {
+            s.append("(" + character + "," + nextStates.get(character).getNumber() + ") ");
+        }
+        s.append(";\n");
+        return s.toString();
+    }
+
+    /**
+     * @return true if this and obj are equivalents
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        if (this == obj) {
+            return true;
+        }
+
+        State state2 = (State) obj;
+        if (this.isFinal ^ state2.isFinal()
+                || this.getNumberOfConnectedStates() != state2.getNumberOfConnectedStates()) {
+            return false;
+        }
+
+        if (getNumberOfConnectedStates() == 0) {
+            return true;
+        }
+
+        for (Character ch : nextStates.keySet()) {
+            if (!nextStates.get(ch).equals(state2.getNextState(ch))) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
 }
